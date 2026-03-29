c8cd6d7349f8a22ddd55eee67a6d6318f2a72c21
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2026-03-30 10:41:00 +1100

    frontend setup

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-red': '#dc2626',
+        'primary-yellow': '#facc15',
+      },
+    },
+  },
+  plugins: [],
+}
