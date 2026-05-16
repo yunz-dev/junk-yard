ebf037d2714ba78b1c2c5ff170c45a130d46b4db
Amy Zhou <37263746+0melette@users.noreply.github.com>
2026-05-16 12:24:32 +1000

    Add user model and seeding
    - Implement User model with fields for username, hashed_password, and created_at.
    -Updated seed to add default users

--- backend/app/models.py
+++ backend/app/models.py
@@ -2,6 +2,14 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from datetime import datetime
 from .database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
 class Flashcard(Base):
     __tablename__ = "flashcards"
 
